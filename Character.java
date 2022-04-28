b210795386c7a60c5f3013bfdc395edfe1895ab6
unknown <narayansurya488@gmail.com>
2022-04-28 14:48:00 +0530

    Second commit

---
+++ Character.java
@@ -0,0 +1,18 @@
+class Character{
+	public static void main(String[] args){
+	char ch='0';
+	if(ch>='0' && ch<='9'){
+	System.out.println("ch contains digit");
+	}
+	else {
+	System.out.println("ch does not contains digit");
+	}
+
+
+
+
+
+
+		}
+
+	}
